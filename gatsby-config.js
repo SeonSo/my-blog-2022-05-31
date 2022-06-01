ae5c8562fde83c7489c7872c6cc71a0582b26933
SeonSo <tjsgh4500@gmail.com>
2022-06-01 13:22:02 +0900

    mdx 플러그인 설치 및 블로그 포스트페이지 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: "My First Gatsby Site",
-    description: "이 사이트는 개발자 소선호의 개발일지 입니다.",
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    "gatsby-plugin-mdx",
+  ],
 };
